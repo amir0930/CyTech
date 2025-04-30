73e7adf0b7c9e0cb3fbb4152d7c939ff95100a32
Achlehi78 <aminelkadicr7@gmail.com>
2025-04-30 15:43:17 +0200

    Update main.c

--- main.c
+++ main.c
@@ -80,43 +80,7 @@ typedef struct {
     char type[20];          // Type de carte (ex: "Offensive", "Défensive")
 } Carte;
 
-//fct pour les cartes
-void utiliserCarte(Carte *carte, Combattant *cible) {
-    printf("Activation de la carte : %s\n", carte->nom);
-    printf("Effet : %s\n", carte->description);
 
-//changer de cartes
-void chargerCartes(char *nomFichier, Carte liste[], int *taille) {
-    FILE *file = fopen(nomFichier, "r");
-    if (file == NULL) {
-        printf("Erreur de lecture du fichier %s\n", nomFichier);
-        return;
-    }
-
-    *taille = 0;
-    while (fscanf(file, "%[^;];%[^;];%d;%d;%s\n", liste[*taille].nom, liste[*taille].description,
-                  &liste[*taille].effet_valeur, &liste[*taille].duree, liste[*taille].type) == 5) {
-        (*taille)++;
-    }
-
-    fclose(file);
-}
-
-//utiliser les cartes
-    printf("3 - Jouer une carte\n");
-
-case 3: {
-    printf("Choisir une carte (0 à %d) : ", tailleCartes - 1);
-    int choixCarte;
-    scanf("%d", &choixCarte);
-
-    if (choixCarte >= 0 && choixCarte < tailleCartes) {
-        utiliserCarte(&cartes[choixCarte], &ordreCombat[i]);
-    } else {
-        printf("Choix de carte invalide.\n");
-    }
-}
-break;
 
 // Appliquer l'effet de la carte
     cible->attaque += carte->effet_valeur;  // Exemple pour un boost d'attaque
@@ -339,13 +303,7 @@ void utiliserCarte(Carte *carte, Combattant *cible) {
            cible->nom, carte->effet_valeur, carte->nom, carte->duree);
 }
 
-void afficherCartesDisponibles(Carte cartes[], int tailleCartes) {
-    printf("Cartes disponibles :\n");
-    for (int i = 0; i < tailleCartes; i++) {
-        printf("%d - %s : %s (Effet : %d, Durée : %d tours)\n", 
-               i, cartes[i].nom, cartes[i].description, cartes[i].effet_valeur, cartes[i].duree);
-    }
-}
+
 
 int main() {
     printf("\033[1;31m--- Combat CY-Fighters ---\033[0m\n");
