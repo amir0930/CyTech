13a535040972af8004141c22fa2b5bfe2d22418d
Achlehi78 <aminelkadicr7@gmail.com>
2025-04-30 15:23:57 +0200

    Update cartes.c

--- cartes.c
+++ cartes.c
@@ -1,14 +1,1 @@
-void attaquer(Combattant *attaquant, Combattant *cible);
-
-void organiserTours(Equipe *equipe1, Equipe *equipe2, Combattant ordreCombat[], int *taille);
-
-void majEffetsActifs(Combattant *cible);
-
-void utiliserTechnique(Combattant *attaquant, Combattant *cible, Technique *technique);
-void majRecharge(Technique *technique);
-
-int choisirCibleAleatoire(Equipe adversaire);
-void attaqueIA(Equipe *equipeIA, Equipe *equipeJoueur, int niveau);
-
-void afficherBarreVieAvecEffets(Combattant *combattant);
 
