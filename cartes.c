ed817cbe7f3416fe29219e5e3ea3621a6d083d7f
Achlehi78 <aminelkadicr7@gmail.com>
2025-04-30 15:49:44 +0200

    Update cartes.c

--- cartes.c
+++ cartes.c
@@ -14,31 +14,18 @@ void chargerCartes(char *nomFichier, Carte liste[], int *taille) {
         printf("Erreur de lecture du fichier %s\n", nomFichier);
         return;
     }
-
     *taille = 0;
-    while (fscanf(file, "%[^;];%[^;];%d;%d;%s\n", liste[*taille].nom, liste[*taille].description,
-                  &liste[*taille].effet_valeur, &liste[*taille].duree, liste[*taille].type) == 5) {
+    while (fscanf(file, "%[^;];%[^;];%d;%d;%s\n", 
+                  liste[*taille].nom, 
+                  liste[*taille].description, 
+                  &liste[*taille].effet_valeur, 
+                  &liste[*taille].duree, 
+                  liste[*taille].type) == 5) {
         (*taille)++;
     }
-
     fclose(file);
 }
 
-//utiliser les cartes
-    printf("3 - Jouer une carte\n");
-
-case 3: {
-    printf("Choisir une carte (0 à %d) : ", tailleCartes - 1);
-    int choixCarte;
-    scanf("%d", &choixCarte);
-
-    if (choixCarte >= 0 && choixCarte < tailleCartes) {
-        utiliserCarte(&cartes[choixCarte], &ordreCombat[i]);
-    } else {
-        printf("Choix de carte invalide.\n");
-    }
-}
-break;
 
 void afficherCartesDisponibles(Carte cartes[], int tailleCartes) {
     printf("Cartes disponibles :\n");
