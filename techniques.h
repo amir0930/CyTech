05525186a64d4cc20a5b2f9bf7e6e8d7da96f460
Achlehi78 <aminelkadicr7@gmail.com>
2025-04-30 15:39:42 +0200

    Create techniques.h

---
+++ techniques.h
@@ -0,0 +1,23 @@
+#ifndef TECHNIQUES_H
+#define TECHNIQUES_H
+#include "structures.h" 
+
+void utiliserTechnique(Combattant *attaquant, Combattant *cible, Technique *technique) {
+    if (technique->recharge > 0) {
+        printf("%s ne peut pas encore utiliser %s (Recharge : %d tours).\n", 
+               attaquant->nom, technique->nom, technique->recharge);
+        return;
+    }
+
+    printf("%s utilise %s sur %s !\n", attaquant->nom, technique->nom, cible->nom);
+    appliquerEffet(cible, technique);
+
+    // Réinitialiser le temps de recharge
+    technique->recharge = 3;  // Par exemple, nécessite 3 tours pour réutilisation
+}
+
+void majRecharge(Technique *technique) {
+    if (technique->recharge > 0) {
+        technique->recharge--;
+    }
+}
