258e5cf1efcdbb49d5b3c1fdb772c25aae1e525e
Achlehi78 <aminelkadicr7@gmail.com>
2025-04-30 15:37:31 +0200

    Update main.c

--- main.c
+++ main.c
@@ -326,20 +326,6 @@ void chargerEquipe(char *nomFichier, Equipe *equipe) {
     printf("Équipe chargée depuis %s\n", nomFichier);
 }
 
-//Techniques spéciales avancées
-void utiliserTechnique(Combattant *attaquant, Combattant *cible, Technique *technique) {
-    if (technique->recharge > 0) {
-        printf("%s ne peut pas encore utiliser %s (Recharge : %d tours).\n", 
-               attaquant->nom, technique->nom, technique->recharge);
-        return;
-    }
-
-    printf("%s utilise %s sur %s !\n", attaquant->nom, technique->nom, cible->nom);
-    appliquerEffet(cible, technique);
-
-    // Réinitialiser le temps de recharge
-    technique->recharge = 3;  // Par exemple, nécessite 3 tours pour réutilisation
-}
 
 
 //Ajoute une logique pour jouer une carte avec ses effets spécifiques
