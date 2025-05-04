296cd22a7eed806212414da062ff3cebdf7813de
amir0930 <amir.azzagagny@gmail.com>
2025-05-04 15:41:32 +0200

    Update cartes.h

--- cartes.h
+++ cartes.h
@@ -1,3 +1,4 @@
+// cartes.h
 #ifndef CARTES_H
 #define CARTES_H
 
@@ -14,32 +15,59 @@ typedef struct {
 
 // Deck (pioche)
 typedef struct {
-    Carte *cards;      // tableau dynamique
+    Carte *cards;      // tableau dynamique de cartes
     int size;          // nombre total de cartes
     int top;           // index de la prochaine carte à piocher
 } Deck;
 
 // Main du joueur
 typedef struct {
-    Carte *cards;      // tableau dynamique
-    int size;          // nb de cartes en main
-    int capacity;      // capacité du tableau
+    Carte *cards;      // tableau dynamique de cartes en main
+    int size;          // nombre de cartes en main
+    int capacity;      // capacité actuelle du tableau
 } Hand;
 
-// Initialisation
+/**
+ * Initialise le deck en chargeant toutes les cartes depuis un fichier CSV.
+ * @param filename   chemin du fichier cartes.txt
+ * @param out_size   retourne le nombre de cartes chargées
+ * @return           pointeur vers un Deck alloué dynamiquement
+ */
 Deck *init_deck(const char *filename, int *out_size);
+
+/**
+ * Mélange le deck (Fisher–Yates) et remet top à 0.
+ */
 void shuffle_deck(Deck *deck);
 
-// Gestion de la main
+/**
+ * Initialise une main vide avec une capacité initiale donnée.
+ */
 Hand *init_hand(int initial_capacity);
+
+/**
+ * Pioche la carte suivante du deck et l'ajoute à la main.
+ */
 void draw_card(Deck *deck, Hand *hand);
-void play_card(Hand *hand, int index, Combattant *cible);
 
-// Nettoyage
-void free_deck(Deck *deck);
-void free_hand(Hand *hand);
+/**
+ * Joue la carte d'indice idx dans la main, en appliquant son effet sur cible.
+ */
+void play_card(Hand *hand, int idx, Combattant *cible);
 
-// Affichage
+/**
+ * Affiche les cartes actuellement en main.
+ */
 void afficher_main(const Hand *hand);
+
+/**
+ * Libère la mémoire allouée au deck.
+ */
+void free_deck(Deck *d);
+
+/**
+ * Libère la mémoire allouée à la main.
+ */
+void free_hand(Hand *h);
 
 #endif // CARTES_H
